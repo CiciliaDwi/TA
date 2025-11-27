97149b3fa45dc0fcc1c6296ffff4a47754b466cd
Alvin Setiawan <alvin.set25@gmail.com>
2025-11-27 20:39:28 +0700

    feat(seeder): add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,25 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::query()->create([
+            'nama' => 'Admin',
+            'tglLahir' => now()->subYears(25),
+            'alamat' => fake()->address,
+            'gaji' => 5000000,
+            'username' => 'admin',
+            'password' => 'Sandi123',
+            'jabatan' => 'admin',
+        ]);
+
+        User::query()->create([
+            'nama' => 'Kasir',
+            'tglLahir' => now()->subYears(25),
+            'alamat' => fake()->address,
+            'gaji' => 5000000,
+            'username' => 'kasir',
+            'password' => 'Sandi123',
+            'jabatan' => 'kasir',
+        ]);
     }
 }
