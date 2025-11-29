785a647103d5137cdf0350cc19a36e9db84b9c03
Alvin Setiawan <alvin.set25@gmail.com>
2025-11-30 04:26:23 +0700

    feat(blade): refactor sidebar.blade.php

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -1,43 +1,47 @@
+@php
+    $currentUser = Auth::user();
+@endphp
+
 <div id="layoutSidenav_nav">
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                @if(Auth::user()->jabatan == 'admin')
+                @if ($currentUser->jabatan == 'admin')
                     <div class="sb-sidenav-menu-heading">Main</div>
-                    <a class="nav-link" href="{{ route('home') }}">
+                    <a href="{{ route('home') }}" @class(['nav-link', 'active' => Route::is('home')])>
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
                     <div class="sb-sidenav-menu-heading">Master Data</div>
-                    <a class="nav-link" href="{{ route('users.index') }}">
+                    <a href="{{ route('users.index') }}" @class(['nav-link', 'active' => Route::is('users.*')])>
                         <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                         Kelola User
                     </a>
-                    <a class="nav-link" href="{{route('products.index')}}">
-                        <div class="sb-nav-link-icon"><i class="fas fa-box"></i></div>
-                        Kelola Produk
-                    </a>
-                    <a class="nav-link" href="{{route('categories.index')}}">
+                    <a href="{{ route('categories.index') }}" @class(['nav-link', 'active' => Route::is('categories.*')])>
                         <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
                         Kelola Kategori
+                    </a>
+                    <a href="{{ route('products.index') }}" @class(['nav-link', 'active' => Route::is('products.*')])>
+                        <div class="sb-nav-link-icon"><i class="fas fa-box"></i></div>
+                        Kelola Produk
                     </a>
                 @endif
 
                 <div class="sb-sidenav-menu-heading">Transaksi</div>
-                <a class="nav-link" href="{{route('transactions.index')}}">
+                <a href="{{ route('transactions.index') }}" @class(['nav-link', 'active' => Route::is('transactions.*')])>
                     <div class="sb-nav-link-icon"><i class="fas fa-shopping-cart"></i></div>
                     Penjualan
                 </a>
 
-                @if(Auth::user()->jabatan == 'admin')
+                @if ($currentUser->jabatan == 'admin')
                     <div class="sb-sidenav-menu-heading">Laporan</div>
-                    <a class="nav-link" href="{{ route('laporan.index') }}">
+                    <a href="{{ route('laporan.index') }}" @class(['nav-link', 'active' => Route::is('laporan.*')])>
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-bar"></i></div>
                         Laporan Penjualan
                     </a>
 
                     <div class="sb-sidenav-menu-heading">Prediksi</div>
-                    <a class="nav-link" href="{{ route('prediction.form') }}">
+                    <a href="{{ route('prediction.form') }}" @class(['nav-link', 'active' => Route::is('prediction.*')])>
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-bar"></i></div>
                         Prediksi Penjualan
                     </a>
@@ -46,7 +50,7 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            {{ Auth::user()->nama }} - {{ Auth::user()->jabatan }}
+            {{ $currentUser->nama }} - {{ $currentUser->jabatan }}
         </div>
     </nav>
 </div>
