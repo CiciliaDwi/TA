c44634fa0447a3bd9e0239ebb91a249bfc52ac09
Alvin Setiawan <alvin.set25@gmail.com>
2025-11-30 10:37:39 +0700

    feat(middleware): fixing middleware routes

--- routes/web.php
+++ routes/web.php
@@ -18,39 +18,45 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::middleware('guest')->group(function () {
+    Route::get('/', function () {
+        return view('auth.login');
+    });
 
-Route::get('/', function () {
-    return view('auth.login');
+    Route::get('/login', [UserController::class, 'showLogin'])->name('login.index');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
 });
 
-Route::get('/login', [UserController::class, 'showLogin'])->name('login.index');
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
-Route::get('/home', [App\Http\Controllers\TransactionController::class, 'dashboard'])->name('home');
+Route::middleware('auth')->group(function () {
 
-Route::get('/users', [UserController::class, 'index'])
-    ->middleware(['auth'])->name('users.index');
+    Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+    Route::get('/home', [App\Http\Controllers\TransactionController::class, 'dashboard'])->name('home');
 
-// pos
-Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
-Route::resource('transactions', TransactionController::class);
-Route::resource('users', UserController::class);
-Route::get('/get-last-nota-number', [TransactionController::class, 'getLastNotaNumber'])->name('get-last-nota-number');
+    Route::get('/users', [UserController::class, 'index'])
+        ->middleware(['auth'])->name('users.index');
 
-// produk
-Route::resource('products', ProductController::class);
-Route::get('/products', [ProductController::class, 'index'])
-    ->middleware(['auth'])->name('products.index');
+    // pos
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::resource('transactions', TransactionController::class);
+    Route::resource('users', UserController::class);
+    Route::get('/get-last-nota-number', [TransactionController::class, 'getLastNotaNumber'])->name('get-last-nota-number');
 
-// kategori
-Route::resource('categories', KategoriController::class);
-Route::get('/categories', [KategoriController::class, 'index'])->name('categories.index');
+    // produk
+    Route::resource('products', ProductController::class);
+    Route::get('/products', [ProductController::class, 'index'])
+        ->middleware(['auth'])->name('products.index');
 
-// report
-Route::get('/laporan', [ReportController::class, 'index'])->name('laporan.index');
+    // kategori
+    Route::resource('categories', KategoriController::class);
+    Route::get('/categories', [KategoriController::class, 'index'])->name('categories.index');
 
-// prediksi
-// Route::get('/prediksi', [ReportController::class, 'index'])->name('laporan.index');
-Route::get('/prediction', [PredictionController::class, 'showForm'])->name('prediction.form');
-Route::post('/prediction', [PredictionController::class, 'predict'])->name('prediction.process');
+    // report
+    Route::get('/laporan', [ReportController::class, 'index'])->name('laporan.index');
+
+    // prediksi
+    // Route::get('/prediksi', [ReportController::class, 'index'])->name('laporan.index');
+    Route::get('/prediction', [PredictionController::class, 'showForm'])->name('prediction.form');
+    Route::post('/prediction', [PredictionController::class, 'predict'])->name('prediction.process');
+    Route::post('/prediction/task', [PredictionController::class, 'getTaskInfo'])->name('prediction.task');
+});
